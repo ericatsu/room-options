d85c3729b53c4ea853787236d56fb4dc9c65da35
ericatsu <ekaatsu@st.ug.edu.gh>
2023-08-11 17:15:51 +0000

    ✨ Setup

---
+++ room_mobile/lib/shared/constants.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class Images {
+  static const String imagesPath = 'assets/images/';
+  static const String logo = '${imagesPath}logo.png';
+}
+
+class Palette {
+  static const Color primary = Color(0xFF2B4BED);
+  static const Color background = Color.fromARGB(255, 252, 252, 252);
+  static const Color textcolor = Color.fromARGB(255, 100, 100, 100);
+  static const Color bordercolor = Color.fromARGB(255, 185, 185, 185);
+  static const Color iconcolor = Color(0xFF0B3E8A);
+}
+
+String compensation_1 = "We provide compensation for damaged items";
+String compensation_2 =
+    "We are ready to compensate if your items are damaged or lost";
+
+String idMessage =
+    "ID Name Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lacus eu eu ornare dui, dolor, egestas adipiscing.";
+String nameMessage =
+    "Item Name Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sit lacus eu eu ornare dui, dolor, egestas adipiscing.";
---
+++ room_mobile/lib/shared/exports.dart
@@ -0,0 +1,9 @@
+//pub
+
+//models
+
+
+//screens
+export 'package:room_mobile/features/home_page.dart';
+
+//common widgets
